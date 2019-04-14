d1537574e28182f5978d52f3821d4b42de8641a4
zhangpinxiu <38860020+zhangpinxiu@users.noreply.github.com>
2019-04-14 14:25:31 +0800

    Update WebUIBase.java

--- src/test/java/com/hogwarts/base/WebUIBase.java
+++ src/test/java/com/hogwarts/base/WebUIBase.java
@@ -59,7 +59,7 @@ public abstract class WebUIBase {
 //            driver=webdriver.Chrome("C:/Program Files (x86)/Google/Chrome/chrome.exe" );
             ChromeOptions options = new ChromeOptions();
             options.setBinary("C:\\Users\\rongrong\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
-            WebDriver dr = new ChromeDriver(options);
+            driver = new ChromeDriver(options);
         } else if (curBrowser.equalsIgnoreCase("phantomjs")) {
             System.setProperty("phantomjs.binary.path", phantomjsPath);
             driver = new PhantomJSDriver();
